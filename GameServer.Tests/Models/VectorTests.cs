48b5ada0e401484c41bc4a8498bfa829740350b6
Pavel Zhukov Alekseevich <zhukowgg@gmail.com>
2026-04-19 17:28:38 +0600

    dd comprehensive ector tests to reach 90% coverage

--- GameServer.Tests/Models/VectorTests.cs
+++ GameServer.Tests/Models/VectorTests.cs
@@ -62,4 +62,85 @@ public class VectorTests
         var b = new Vector(1, 2, 3);
         Assert.Equal(a.GetHashCode(), b.GetHashCode());
     }
+
+    [Fact]
+    public void Constructor_WithNullArray_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new Vector(null!));
+    }
+
+    [Fact]
+    public void Indexer_WithNegativeIndex_ThrowsIndexOutOfRangeException()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.Throws<IndexOutOfRangeException>(() => vector[-1]);
+    }
+
+    [Fact]
+    public void Indexer_WithIndexEqualToLength_ThrowsIndexOutOfRangeException()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.Throws<IndexOutOfRangeException>(() => vector[3]);
+    }
+
+    [Fact]
+    public void ToString_ReturnsCorrectFormat()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.Equal("(1, 2, 3)", vector.ToString());
+    }
+
+    [Fact]
+    public void Equals_WithNullObject_ReturnsFalse()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.False(vector.Equals(null));
+    }
+
+    [Fact]
+    public void Equals_WithNonVectorObject_ReturnsFalse()
+    {
+        var vector = new Vector(1, 2, 3);
+        Assert.False(vector.Equals("not a vector"));
+    }
+
+    [Fact]
+    public void Equals_WithDifferentDimensions_ReturnsFalse()
+    {
+        var a = new Vector(1, 2);
+        var b = new Vector(1, 2, 3);
+        Assert.False(a.Equals(b));
+    }
+
+    [Fact]
+    public void EqualityOperator_WithNullLeft_ReturnsFalse()
+    {
+        Vector? a = null;
+        var b = new Vector(1, 2, 3);
+        Assert.False(a == b);
+    }
+
+    [Fact]
+    public void EqualityOperator_WithBothNull_ReturnsTrue()
+    {
+        Vector? a = null;
+        Vector? b = null;
+        Assert.True(a == b);
+    }
+
+    [Fact]
+    public void InequalityOperator_WithNullLeft_ReturnsTrue()
+    {
+        Vector? a = null;
+        var b = new Vector(1, 2, 3);
+        Assert.True(a != b);
+    }
+
+    [Fact]
+    public void GetHashCode_WithDifferentComponents_ReturnsDifferentHash()
+    {
+        var a = new Vector(1, 2, 3);
+        var b = new Vector(4, 5, 6);
+        Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+    }
 }
