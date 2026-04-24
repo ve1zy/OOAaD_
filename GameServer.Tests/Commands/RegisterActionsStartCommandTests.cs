9db05a9a32dd1128d66cd7369d9cd6933c4ce3ea
Pavel Zhukov Alekseevich <zhukowgg@gmail.com>
2026-04-24 19:35:44 +0600

    Task 18: Implement StartCommand and register Actions.Start dependency in IoC with tests

---
+++ GameServer.Tests/Commands/RegisterActionsStartCommandTests.cs
@@ -0,0 +1,36 @@
+#nullable disable
+using GameServer.Commands;
+using GameServer.Interfaces;
+using Xunit;
+
+namespace GameServer.Tests.Commands;
+
+public class RegisterActionsStartCommandTests
+{
+    [Fact]
+    public void Execute_RegistersActionsStartInIoC()
+    {
+        IoC.Ioc.Clear();
+        var mockCommand = new MockCommand();
+        IoC.Ioc.Register("TestCommand", (args) => mockCommand);
+        
+        var registerCommand = new RegisterActionsStartCommand();
+        registerCommand.Execute();
+        
+        var startCommand = (StartCommand)IoC.Ioc.Resolve("Actions.Start", "TestCommand");
+        Assert.NotNull(startCommand);
+        
+        startCommand.Execute();
+        Assert.True(mockCommand.Executed);
+    }
+
+    private class MockCommand : ICommand
+    {
+        public bool Executed { get; private set; }
+        
+        public void Execute()
+        {
+            Executed = true;
+        }
+    }
+}
