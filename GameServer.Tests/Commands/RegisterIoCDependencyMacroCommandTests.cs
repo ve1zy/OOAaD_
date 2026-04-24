e6ac5153af5d49726099cb3321981a2392bc104a
Pavel Zhukov Alekseevich <zhukowgg@gmail.com>
2026-04-24 19:19:39 +0600

    Task 11: Register Commands.Macro dependency in IoC with RegisterIoCDependencyMacroCommand

---
+++ GameServer.Tests/Commands/RegisterIoCDependencyMacroCommandTests.cs
@@ -0,0 +1,38 @@
+#nullable disable
+using GameServer.Commands;
+using Xunit;
+
+namespace GameServer.Tests.Commands;
+
+public class RegisterIoCDependencyMacroCommandTests
+{
+    [Fact]
+    public void Execute_RegistersMacroCommandInIoC()
+    {
+        IoC.Ioc.Clear();
+        var mockCommand1 = new MockCommand();
+        var mockCommand2 = new MockCommand();
+        IoC.Ioc.Register("TestCommand1", (args) => mockCommand1);
+        IoC.Ioc.Register("TestCommand2", (args) => mockCommand2);
+        
+        var registerCommand = new RegisterIoCDependencyMacroCommand();
+        registerCommand.Execute();
+        
+        var macroCommand = (MacroCommand)IoC.Ioc.Resolve("Commands.Macro", "TestCommand1", "TestCommand2");
+        Assert.NotNull(macroCommand);
+        
+        macroCommand.Execute();
+        Assert.True(mockCommand1.Executed);
+        Assert.True(mockCommand2.Executed);
+    }
+
+    private class MockCommand : ICommand
+    {
+        public bool Executed { get; private set; }
+        
+        public void Execute()
+        {
+            Executed = true;
+        }
+    }
+}
