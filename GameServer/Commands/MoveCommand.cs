d3a05bdc83945ed2be2453fbe4450eac6986d35a
User <user@example.com>
2026-04-25 10:59:01 +0600

    Fix Vector reference in move interfaces

--- GameServer/Commands/MoveCommand.cs
+++ GameServer/Commands/MoveCommand.cs
@@ -1,13 +1,14 @@
 using GameServer.Interfaces;
+using GameServer.Models;
 
 namespace GameServer.Commands;
 
 public class MoveCommand : ICommand
 {
     private readonly IMovingObject _movableObject;
-    private readonly Vector _position;
+    private readonly GameServer.Models.Vector _position;
 
-    public MoveCommand(IMovingObject movableObject, Vector position)
+    public MoveCommand(IMovingObject movableObject, GameServer.Models.Vector position)
     {
         _movableObject = movableObject;
         _position = position;
